407c88e57ce1a25dec2a4ba4524d296275c3b76a
Conan Leong <conanaleong@gmail.com>
2022-06-20 17:37:08 -0700

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,288 @@
-import logo from './logo.svg';
+import { click } from '@testing-library/user-event/dist/click';
+import React, { Component, useState } from 'react';
+import { isDOMComponent } from 'react-dom/test-utils';
 import './App.css';
 
-function App() {
+var _ = require('lodash');
+
+var todoList = JSON.parse(localStorage.getItem("todoList") || "[]");
+var displayAll = false
+
+for (let i = 0; i < todoList.length; i++) {
+  if (todoList[i].deleted == true) {
+    todoList.splice(i, 1);
+  }}
+
+class Todo {
+  constructor(name, description, priority) {
+  this.idee = Date.now()
+  this.date = new Date();
+
+  this.done = false;
+  this.deleted = false;
+
+  this.name = name;
+  this.description = description; //Todo: Add support for sub lists and stuff
+  this.priority = priority;
+
+  todoList.push(this)
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  console.log("ID is " + this.idee)
+  }
+}
+
+if (todoList == []) {
+  const placeholder1 = new Todo("Wash Dishes", "Ya gotta soap them and rinse them and all that", 2)
+  const placeholder2 = new Todo("Walk Dog", "Ya gotta soap them and rinse them and all that", 2)
+  const placeholder3 = new Todo("Eat Cheese", "Ya gotta soap them and rinse them and all that", 2, true)
+}
+
+
+
+class TodoCard extends React.Component {
+constructor (props) {
+  super(props);
+
+  this.toggledone = this.toggledone.bind(this);
+  this.expand = this.expand.bind(this);
+  this.remove = this.remove.bind(this);
+  this.refresh = this.props.refresh.bind(this);
+
+  this.state = {
+    todo: this.props.todo,
+    expand: false,
+  };
+}
+
+
+ remove() {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].idee == this.state.todo.idee) {
+      console.log("deleted " + todoList[i].name)
+
+      if (todoList[i].deleted == true) {
+        todoList[i].deleted = false;
+      } else {
+        todoList[i].deleted = true;
+      }
+
+      localStorage.setItem("todoList", JSON.stringify(todoList))
+      this.props.refresh()
+      break;
+    }
+  }
+}
+
+ expand() {
+  console.log('clicked')
+  if (this.state.expand == true) {
+   this.setState({expand: false});
+  } else {
+   this.setState({expand: true});
+  }
+}
+
+toggledone() {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].idee == this.state.todo.idee) {
+      console.log("toggle " + todoList[i].name)
+
+      if (todoList[i].done == true) {
+        todoList[i].done = false;
+      } else {
+        todoList[i].done = true;
+      }
+
+      localStorage.setItem("todoList", JSON.stringify(todoList))
+      this.props.refresh()
+      break;
+    }
+  }
+}
+
+ render() {
+  const {name, description, priority, done, date, idee} = this.state.todo;
+  const expand = this.state.expand;
+
+  let stringArray = (date.toString()).split("");   // spliting string into array of characters
+  let displayDate = stringArray.slice(0, 10).join("")
+  console.log(displayDate)
+
+  if (expand == false) {
+   return (
+     <div className='todoCard' >
+       <div className="todoTopBar" >
+        <div className={ done ? 'done-button done' : 'done-button notdone' } onClick={ ()=>this.toggledone()}></div>
+        <div className="todo-card-title" onClick={this.expand}>{name}</div>
+       </div>
+     </div>
+   )} else {
+    return (
+      <div className='todoCard' >
+      <div className="todoTopBar" >
+      <div className={ done ? 'done-button done' : 'done-button notdone' } onClick={ ()=>this.toggledone()}></div>
+      <div className="todo-card-title" onClick={this.expand}>{name}</div>
+      </div>
+      <div>{description}</div>
+      <div>{priority} Priority</div>
+      <div>{displayDate}</div>
+      <span id="removeTodo" onClick={ ()=>this.remove()} className="btn delete-todo">DELETE</span>
+    </div>
+   )}
+ }
+}
+
+class NewTodoExpansion extends React.Component {
+
+  render() {
+    return (
+      <div >
+
+        <div className="new-todo-row">
+         <label htmlFor="entry-description">Description: </label>
+         <input type="text" id="entry-description" />
+        </div>
+    
+      <div className="new-todo-row">     
+        <input type="radio" name="Priority" id="Low" value="Low"/>
+        <label htmlFor="Low">Low</label>
+
+        <input type="radio" name="Priority" id="Medium"  value="Medium" checked/>
+        <label htmlFor="Medium">Medium</label>
+
+        <input type="radio" name="Priority" id="High" value ="High" />
+        <label htmlFor="High">High</label>
+      </div>
+
+      </div>
+
+    )
+  }
+}
+
+class NewTodo extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      expand: false
+    };
+
+    this.addItem = this.props.addItem.bind(this);
+    this.expand = this.expand.bind(this);
+    this.collapse = this.collapse.bind(this);
+    this.addHandle = this.addHandle.bind(this);
+}
+
+expand() {
+   this.setState({expand: true});
+}
+
+collapse() {
+  this.setState({expand: false});
+}
+
+addHandle() {
+  this.addItem()
+  this.collapse()
+}
+
+render() {
+
+  if (this.state.expand == false ) {
+  return (
+    <div className="newtodo">
+        <h2>New Todo</h2>
+        <div className="new-todo-form">
+          <div className="new-todo-row">
+            <label htmlFor="title">Title: </label>
+            <input name="title" type="text" id="entry" onFocus={this.expand} />
+          </div>
+
+          <button type="submit" onClick={this.addHandle}>Add</button>
+        </div>
+    </div>
+  )} else {
+    return (
+      <div className="newtodo">
+          <h2>New Todo</h2>
+          <div className="new-todo-form">
+            <div>
+              <label htmlFor="title">Title: </label>
+              <input name="title" type="text" id="entry" />
+            </div>
+            <NewTodoExpansion />
+            <button type="submit" onClick={this.addHandle}>Add</button>
+          </div>
+      </div>
+    )}
+}
+
+}
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      todoList: todoList
+    };
+
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+    this.refresh = this.refresh.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem() {
+    let newTitle = document.getElementById("entry").value
+    if (newTitle == "") return alert("Please enter a title")
+    if (newTitle.length > 18) return alert("Title must not exceed 18 characters")
+    let newDescription = document.getElementById("entry-description").value
+    let newPriority = document.querySelector('input[name=Priority]:checked').value
+    new Todo(newTitle, newDescription, newPriority)
+    document.getElementById("entry").value = ""
+    this.refresh()
+  }
+
+  toggleDisplay() {
+    displayAll = displayAll ? false : true 
+    this.refresh()
+  }
+
+  refresh() {
+    this.setState({todoList: todoList});
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
+        <div className="title" id="title"><h1>React To Do List</h1></div>
+        <div className="description" id="description">Add stuff to todo list and check off completed items</div>
+        {/* <button onClick={()=>{console.log(todoList)}}>todoList wat</button> */}
       </header>
+
+      <button className="displayAll" id="displayAll" onClick={this.toggleDisplay}>{displayAll? "Show Unfinished" : "Show All"} </button>
+
+      <div className="todoTable" id="todoTable">
+
+        {todoList.map((e)=>{
+            if (displayAll == false && e.done) return
+            if (e.deleted) return
+            return (
+             <TodoCard todo={e} refresh={this.refresh} /> 
+            );
+          }
+        )}
+     
+      </div>
+
+      <NewTodo addItem={this.addItem}  />
+
     </div>
   );
+}
 }
 
 export default App;
